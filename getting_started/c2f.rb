0dd250fb5e5bf40f20a66f513f6603152d565922
Mickael Pham <mickael.pham@outlook.com>
2014-07-19 19:11:12 -0700

    Writing results to file

--- getting_started/c2f.rb
+++ getting_started/c2f.rb
@@ -1,6 +1,7 @@
 print "Enter the temperature in Celsius: "
 celsius = gets.chomp.to_i
 fahrenheit = (celsius * 9) / 5 + 32
-print "The result is: "
-print fahrenheit
-puts "."
+puts "Saving result to output file 'temp.txt'"
+fh = File.new("temp.txt", "w")
+fh.puts fahrenheit
+fh.close
