cb11ef5b1e9b027d23946d5417c850fb0b2336ea
Mickael Pham <git@mpham.eu>
2014-07-20 23:05:45 -0700

    Modified comment

--- be_classy/ticket.rb
+++ be_classy/ticket.rb
@@ -20,5 +20,5 @@ t = Ticket.new("Town Hall", "Cascada LIVE!")
 t.price=(63.00)
 puts "The price for the ticket is #{"$%.2f" % t.price}"
 
-t.price = 72.00 # Syntactic sugar
+t.price = 72.00 # Syntactic sugar for assignment
 puts "--- Woops, it's now #{"$%.2f" % t.price}"
