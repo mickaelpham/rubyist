bc9ece0df9ea21307933ed9f5f0b2a41f401e073
Mickael Pham <git@mpham.eu>
2014-07-20 22:53:34 -0700

    Started ticket class

---
+++ be_classy/ticket.rb
@@ -0,0 +1,24 @@
+class Ticket
+
+  def initialize(venue, event)
+    @venue = venue
+    @event = event
+  end
+
+  def price=(amount)
+    @price = amount
+  end
+
+  def price
+    @price
+  end
+
+end
+
+t = Ticket.new("Town Hall", "Cascada LIVE!")
+
+t.price=(63.00)
+puts "The price for the ticket is #{"$%.2f" % t.price}"
+
+t.price = 72.00 # Syntactic sugar
+puts "--- Woops, it's now #{"$%.2f" % t.price}"
